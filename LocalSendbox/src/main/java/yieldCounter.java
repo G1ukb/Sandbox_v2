e8186ec913193eb2cc39c55deffa7fed47336903
Hlebovski <glukb1@gmail.com>
2022-05-05 12:30:38 +0300

    Updated yieldCounter.java

--- LocalSendbox/src/main/java/yieldCounter.java
+++ LocalSendbox/src/main/java/yieldCounter.java
@@ -1,24 +1,31 @@
 public class yieldCounter {
 
     public static void main(String[] args) {
-        Double count = 0.1344;
-        Double startPrice = 0.07173663;
-        Boolean isLong = false;
-        Long margin = 2L;
-        Double percent = 0.02;
+        Double count = 66.91;
+        Double startPrice = 40.85;
+        Double currPrice = 32.27;
+        Boolean isLong = true;
+        Long margin = 3L;
+        Double percent = 0.01;
 
         Counter counter = new Counter();
-        counter.priceCounter(count,startPrice, isLong, margin, percent);
+        counter.priceCounter(count,startPrice, currPrice, isLong, margin, percent);
     }
 
 }
 
 class Counter {
-    void priceCounter(Double count, Double startPrice, Boolean isLong, Long margin, Double percent) {
+    void priceCounter(Double count, Double startPrice, Double currPrice, Boolean isLong, Long margin, Double percent) {
         double pricePercent = 0.05;
         for (double i = percent; i <= percent * 5; i = i + percent) {
             System.out.println("============" + i * 100 + "%%");
-            System.out.println(isLong ? startPrice + (startPrice * (i / margin)) : startPrice - (startPrice * (i / margin)));
+
+            double cellPrice = isLong ? startPrice + (startPrice * (i / margin)) : startPrice - (startPrice * (i / margin));
+            if (isLong){
+                System.out.println(cellPrice < currPrice ? "Sell in market (" + cellPrice + ")" : cellPrice);
+            } else {
+                System.out.println(cellPrice > currPrice ? "Sell in market (" + cellPrice + ")" : cellPrice);
+            }
             System.out.println(count * pricePercent);
 
             pricePercent += 0.05;
